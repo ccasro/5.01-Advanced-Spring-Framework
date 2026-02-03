0ed66dd5901dbd8c7dcaee442e54dc51cf6a4ad8
ccasro <ccasro@gmail.com>
2026-02-03 11:03:51 +0100

    chore: add SpringBoot content

---
+++ src/main/java/cat/itacademy/s05/t01/blackjack_api/BlackjackApiApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.t01.blackjack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackApiApplication.class, args);
+	}
+
+}
