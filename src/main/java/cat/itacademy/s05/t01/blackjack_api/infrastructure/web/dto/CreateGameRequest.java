057cd71f9c1105c5177701d57784567098967b88
Cesar <ccasro@gmail.com>
2026-02-05 08:21:16 +0100

    feat(web): add POST /game/new endpoint

---
+++ src/main/java/cat/itacademy/s05/t01/blackjack_api/infrastructure/web/dto/CreateGameRequest.java
@@ -0,0 +1,7 @@
+package cat.itacademy.s05.t01.blackjack_api.infrastructure.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateGameRequest(@NotBlank(message = "Player name must not be blank") @Size(max = 30, message = "Player name length must be <= 30") String playerName) {
+}
