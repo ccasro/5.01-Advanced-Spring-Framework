55135e059e6910bef4ca0404f633862ecf2842d7
ccasro <ccasro@gmail.com>
2026-02-05 13:43:26 +0100

    chore(config): wire GetGameStateUseCase bean

--- src/main/java/cat/itacademy/s05/t01/blackjack_api/infrastructure/config/UseCaseConfig.java
+++ src/main/java/cat/itacademy/s05/t01/blackjack_api/infrastructure/config/UseCaseConfig.java
@@ -1,6 +1,8 @@
 package cat.itacademy.s05.t01.blackjack_api.infrastructure.config;
 
+import cat.itacademy.s05.t01.blackjack_api.application.mapper.GameStateMapper;
 import cat.itacademy.s05.t01.blackjack_api.application.usecase.CreateNewGameUseCase;
+import cat.itacademy.s05.t01.blackjack_api.application.usecase.GetGameStateUseCase;
 import cat.itacademy.s05.t01.blackjack_api.domain.port.GameRepositoryPort;
 import cat.itacademy.s05.t01.blackjack_api.domain.port.PlayerRepositoryPort;
 import org.springframework.context.annotation.Bean;
@@ -12,5 +14,10 @@ public class UseCaseConfig {
     @Bean
     CreateNewGameUseCase createNewGameUseCase(PlayerRepositoryPort playerRepo, GameRepositoryPort gameRepo) {
         return new CreateNewGameUseCase(playerRepo,gameRepo);
+    }
+
+    @Bean
+    GetGameStateUseCase getGameStateUseCase(GameRepositoryPort gameRepo){
+        return new GetGameStateUseCase(gameRepo, new GameStateMapper());
     }
 }
